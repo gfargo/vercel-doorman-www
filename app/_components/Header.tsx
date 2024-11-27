2d9b08b0f2f4bfbb745a68d3998b4be88017d633
Griffen Fargo <ghfargo@gmail.com>
2024-11-26 23:31:45 -0500

    Add npm link and icon to header
    Enhance the `Header` component by adding a new link to the npm package for `vercel-doorman`, complete with an `NpmIcon`. This addition improves navigation by providing direct access to the npm page. Also, correct a minor typo in the GitHub link class name to ensure consistent styling.

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -15,6 +15,15 @@ export const Header = () => {
           <div className="flex items-center space-x-8">
             <div className="flex items-center space-x-4">
               <Link
+                href="https://npmjs.com/package/vercel-doorman"
+                className="text-gray-600 hover:text-black transition-colors mr-6"
+              >
+                <div className="w-10 mt-1 flex items-center justify-center">
+                  <NpmIcon />
+                </div>
+                <span className="sr-only">npm</span>
+              </Link>
+              <Link
                 href="/docs"
                 className="text-gray-600 hover:text-black transition-colors"
               >
@@ -28,7 +37,7 @@ export const Header = () => {
               </Link>
               <Link
                 href="https://github.com/gfargo/vercel-doorman"
-                className="text-gray-600 hover:text-black transition-colors"
+                className="text-gray-600 hover:text-black transition-colorzs"
               >
                 <Github className="w-6 h-6" />
                 <span className="sr-only">GitHub</span>
@@ -38,5 +47,34 @@ export const Header = () => {
         </nav>
       </div>
     </header>
+  );
+};
+
+const NpmIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 18 7"
+    >
+      <path
+        fill="#CB3837"
+        d="M0,0h18v6H9v1H5V6H0V0z M1,5h2V2h1v3h1V1H1V5z M6,1v5h2V5h2V1H6z M8,2h1v2H8V2z M11,1v4h2V2h1v3h1V2h1v3h1V1H11z"
+      />
+      <polygon
+        fill="#FFFFFF"
+        points="1,5 3,5 3,2 4,2 4,5 5,5 5,1 1,1 "
+      />
+      <path
+        fill="#FFFFFF"
+        d="M6,1v5h2V5h2V1H6z M9,4H8V2h1V4z"
+      />
+      <polygon
+        fill="#FFFFFF"
+        points="11,1 11,5 13,5 13,2 14,2 14,5 15,5 15,2 16,2 16,5 17,5 17,1 "
+      />
+    </svg>
   );
 };
